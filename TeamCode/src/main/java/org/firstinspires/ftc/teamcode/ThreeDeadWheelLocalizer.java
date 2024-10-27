e3b3b360dbeab54198f09c69261439b43ffa623c
Brandon Wang <brandon16.wang@gmail.com>
2024-10-27 14:12:59 -0700

    finished tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
@@ -20,8 +20,8 @@ import org.firstinspires.ftc.teamcode.messages.ThreeDeadWheelInputsMessage;
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks = -2167.539712124382; // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 2032.603284748717; // y position of the second parallel encoder (in tick units)
+        public double par0YTicks = -2100.071498; // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = 2100.071498; // y position of the second parallel encoder (in tick units)
         public double perpXTicks = -2202.637659899079; // x position of the perpendicular encoder (in tick units)
     }
 
